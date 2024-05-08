16fa17856f3789b0614211dcb4ee93530038befb
DaliAbess <daliabess51@gmail.com>
2024-05-08 00:13:35 +0000

    add matiere entity for rest api

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -3,7 +3,9 @@ package com.example.demo.controller;
 
 
 import com.example.demo.model.User;
+import com.example.demo.model.Matiere;
 import com.example.demo.service.UserService;
+import com.example.demo.service.MatiereService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,21 +17,37 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-
+    @Autowired
+    private MatiereService matiereService;
     @GetMapping
-    public List<User> getAllBooks() {
+    public List<User> getUsers() {
         return userService.getAllUsers();
     }
 
     @GetMapping("/{id}")
-    public User getBookById(@PathVariable Long id) {
+    public User getUserById(@PathVariable Long id) {
         return userService.getUserById(id);
     }
 
     @PostMapping
-    public User saveBook(@RequestBody User user) {
+    public User saveUser(@RequestBody User user) {
         return userService.saveUser(user);
     }
+//matieres
+@GetMapping("/{id}/matieres")
+public List<Matiere> getMatiereForUser(@PathVariable Long id) {
+    User user=userService.getUserById(id);
+    List<Matiere> matieres = user.getMatieres();
+    return matieres ;
+}
+
+@PostMapping("/{id}/matieres")
+public void saveMatiereForUser(@PathVariable Long id , @RequestBody Matiere matiere ) {
+    User user=userService.getUserById(id);
+    matiere.setUser(user);
+    matiereService.saveMatiere(matiere);
+    
+}
 
     @DeleteMapping("/{id}")
     public void deleteBook(@PathVariable Long id) {
--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,12 +1,21 @@
 package com.example.demo.model;
-
+import java.util.HashSet;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList; 
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 @Entity 
 @Table(name="user")
 public class User {
@@ -15,6 +24,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     
+    public User(String name, String cin, Role role) {
+        this.name = name;
+        this.cin = cin;
+        this.role = role;
+        
+    }
     public User() {
     }
 
@@ -22,14 +37,26 @@ public class User {
     @Column(name="name",nullable=false )
     private String name;
     @Column(name="cin",nullable=false )
-    private String cin;
+    private String cin ;
     @Column(name="role",nullable=false )
     private Role role;
+    @JsonManagedReference
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Matiere> matieres ;
     // Getters and Setters
 
     public Long getId() {
         return id;
     }
+
+    public void setMatieres(List<Matiere> matieres) {
+        this.matieres = matieres;
+    }
+    public List<Matiere> getMatieres() {
+        return matieres;
+    }
+
+    
     public String getName() {
         return name;
     }
